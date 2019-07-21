ab461dddd2bf116883366dbea0b5188a42fefa29
Wiktor <wiktormax2@gmail.com>
2019-07-21 17:18:58 +0100

    connected to nasa api

--- src/router.js
+++ src/router.js
@@ -1,18 +1,16 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+import Search from './views/Search.vue';
 import About from './views/About.vue';
 
 Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
-      name: 'Home',
-      component: Home,
+      name: 'Search',
+      component: Search,
     },
     {
       path: '/about',
